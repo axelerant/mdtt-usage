13104a97c8fd7377b779c85d192d0417869ce4dc
Subhojit Paul <subhojitpaul21@gmail.com>
2022-03-28 16:26:07 +0530

    init

---
+++ tests/mdtt/spec.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'databases' => [
+        'source_db' => [
+            'database' => "db_drupal7",
+            'username' => "db",
+            'password' => "db",
+            'host' => "127.0.0.1",
+            'port' => "59002",
+        ],
+        "destination_db" => [
+            'database' => "db",
+            'username' => "db",
+            'password' => "db",
+            'host' => "127.0.0.1",
+            'port' => "59002",
+        ],
+    ]
+];
